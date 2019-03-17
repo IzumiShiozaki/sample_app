43b7e974a1d05e74e23a7fc41de7b4874154f9d8
IzumiShiozaki <over.drive.0108@outlook.jp>
2019-03-17 17:05:58 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,12 +6,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post signup_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    # CSSのerror_explanationというidがあるかどうか
+    assert_select 'div#error_explanation'
+    # _error_messagesパーシャルにalertというクラスが設定されているかどうか
+    assert_select 'div.alert'
+    assert_select 'form[action="/signup"]'
   end
 
   #有効なユーザー登録に対するテスト
@@ -27,6 +32,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_not flash.empty?
   end
+  
+ 
+
 
 end
 
